7b15f3fca5b9fc0ff0ff59280874d475e6063d0f
prakharpbuf <prakhar.paw08@gmail.com>
2020-11-05 14:08:24 -0500

    Added favicon and to not allow codes after prof disallows codes but use has already opeaned landing.ejs

--- routes/submit.js
+++ routes/submit.js
@@ -10,17 +10,25 @@ router.post('/submit', function(req,res,next){
     var firstName = req.sanitize("first-name").escape().trim()
     var lastName = req.sanitize("last-name").escape().trim()
     var code = req.sanitize("code").escape().trim()
-    connection.query('INSERT INTO codes (firstName, lastName, code) VALUES ($1, $2, $3);', [firstName, lastName, code], function (err, result) {
-        //if(err) throw err
-        if (err) {
-            req.flash('error', err)
-            return res.render('landing')
-        } else {
-            req.session.marked=1;
-            res.redirect('thanks');
+    connection.query('Select allowed from allow', function (err, result) {
+        if(!result.rows[0].allowed){
+            console.log(result.rows);
+            return res.sendFile(parent+'/public/too-late.html')
         }
-    });
-    
+        else{
+            connection.query('INSERT INTO codes (firstName, lastName, code) VALUES ($1, $2, $3);', [firstName, lastName, code], function (err, result) {
+                //if(err) throw err
+                if (err) {
+                    req.flash('error', err)
+                    return res.render('landing')
+                } else {
+                    req.session.marked = 1;
+                    res.redirect('thanks');
+                }
+            });
+        }
+    })
+     
 })
 
 module.exports = router;
